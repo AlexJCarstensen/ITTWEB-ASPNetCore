169308dd2a87c15a24b0ad0c215705365888026b
Alex <alex.jul.carstensen@gmail.com>
2016-11-30 17:57:46 +0100

    added connection from dbcontext to homecontroller

--- ITTWEB-ASPNetCore/Controllers/HomeController.cs
+++ ITTWEB-ASPNetCore/Controllers/HomeController.cs
@@ -2,25 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ITTWEB_ASPNetCore.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ITTWEB_ASPNetCore.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Category()
         {
+            var categories = _context.Catagories.ToList();
+
+            //viewmodel
+
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Component()
         {
             ViewData["Message"] = "Your contact page.";
 
