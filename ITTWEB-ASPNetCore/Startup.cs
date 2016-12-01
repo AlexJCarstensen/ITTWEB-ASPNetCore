c9c96146341fab688143dfd1443a10407943b22c
AlexCarstensen <alex.jul.carstensen@gmail.com>
2016-12-01 11:25:55 +0100

    added sqlserver to project.json and set connection string

--- ITTWEB-ASPNetCore/Startup.cs
+++ ITTWEB-ASPNetCore/Startup.cs
@@ -41,7 +41,7 @@ namespace ITTWEB_ASPNetCore
         {
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
